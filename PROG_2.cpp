5b575bf328297a089a543ca5e19ab0294a3ba4d5
oscarpartido <oscarpartido01@gmail.com>
2022-05-19 13:35:14 -0500

    commit cliente
    agregamos el archivo actividad 2 realizada en clase, poniendo  nuestros datos personales.

---
+++ PROG_2.cpp
@@ -0,0 +1,36 @@
+/* Autor: Oscar Vivaldi Partido Ramirez, realizado: 03/02/2022
+
+programa que pide los datos de un triangulo y calcule su area
+*/
+
+#include <stdio.h>
+
+int main(){
+
+//declaracion de variables
+
+int base,altura,area;
+
+//entrada de datos
+
+printf("calcular el area del triangulo \n");
+
+printf("introduce la base:");
+
+scanf("%d",&base);
+
+printf("introduce la altura:");
+
+scanf("%d",&altura);
+
+//proceso
+
+area=base*altura/2;
+
+//salida
+
+printf("el area del triangulo es %d",area);
+
+return 0;
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PROG_EST_ACT2
-programa que muestra la estructura básica de un programa en el lenguaje C , programa que pide los datos de un triangulo y calcule su area
+Autor:oscar vivaldi partido ramirez 
+escuela: uvm campus Queretaro
+materia: Programacion Estructurada
+ciclo:01/2022
+programa que muestra la estructura básica de un programa en el lenguaje C,
+programa que pide los datos de un triangulo y calcule su area.
